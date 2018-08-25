d1c8d83487705ff132f48d5f43ac081afa2feaa2
Regina Nurieva <regginurieva@gmail.com>
2018-08-24 22:00:05 -0700

    program is not functioning

--- pizza_order/js/scripts.js
+++ pizza_order/js/scripts.js
@@ -1,17 +1,13 @@
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = [];
+  this.toppings = toppings;
+  // this.price;
 };
-
-//to determine the pizza price
-
-// if(this.size==="Medium"){
-//   this.size += this.toppings.lenght
-//
-// }
-
-Pizza.prototype.price=function(){
-  price = 10;
+// var pizzaSize = {"medium":10, "large":10, "family":10};
+// var pizzaTopping = {"olives":1, "mushrooms":1, "onion":1};
+//to determine the pizza prices
+Pizza.prototype.price = function(){
+var price = 10;
 
   if(this.size === "Family"){
     price +=3;
@@ -20,21 +16,45 @@ Pizza.prototype.price=function(){
   } else {
     price +=1;
   }
+// }
+//toppings price
+if (this.toppings===1){
+  price +=1;
+} else{
+  price += 0;
+}
+ //return price;
+ alert("bjh")
 }
 
-
-
 $(document).ready(function() {
-$("form#pizza-order-form").submit(function(event){
-  event.PreventDefault();
+  $("form#order").submit(function(event){
+    $("#pizza-size").text("code");
+    //$("#pizza-size").text("Before prevent");
+  // event.PreventDefault();
+  // $("#pizza-size").text("code in the form");
+  // alert("tj");
 
+});
+//
   var pizzaSize = $("input:radio[name='size']:checked").val();
+  var newPizza = new Pizza(pizzaSize);
+  // $("#pizza-size").text("Before prevent");
+//
+//   $.each($("input[name='toppings']:checked"),function(){
+//     newPizza.toppings.push($(this).val());
+//   });
+// // console.log("#submitBtn")
+ // $("button#submitBtn").click(function(){
+//   // $(".pizzaOrder").last().click(function(){
+//   // $("#pizza-order-detail").show();
 
+//   // $("#pizza-size").text(newPizza.size);
+// $("#pizza-ingredients").text(newPizza.toppings);
+//   $("#order-total").text(newPizza.price());
+//   // console.log();
 
-
-});
-
-
-
-
-});
+// });
+ // });
+// debugger;
+ });
